f336f1461ff3e21f5c1a6707fa6463aa60285c00
Grady L Shelton <gradyish@gmail.com>
2017-05-12 12:51:00 -0700

    pass integration test for updating store name

--- spec/shoe_assist_integration_spec.rb
+++ spec/shoe_assist_integration_spec.rb
@@ -40,3 +40,13 @@ describe('viewing a brand', {:type => :feature}) do
     expect(page).to have_content('Nicke Air Gordon')
   end
 end
+
+describe('updating store name', {:type => :feature}) do
+  it('allows a user change name of a store') do
+    store = Store.create({name: "shoe fly guys"})
+    visit("/store/#{store.id}")
+    fill_in('store_name', :with => 'Footies R Us')
+    click_button('edit store name')
+    expect(page).to have_content('Footies R Us')
+  end
+end
