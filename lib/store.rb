213013b73dad99c21ee7f5699d0f947f51efb7ac
Grady L Shelton <gradyish@gmail.com>
2017-05-12 08:59:16 -0700

    pass tests for validation of a name being present for both store and brand creation

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,29 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
+  validates(:name, {presence: true, length: { maximum: 100}})
+  before_save(:titlecase)
+
+private
+  define_method(:titlecase) do
+    words = self.title.split(" ")
+    words.each do |word|
+      word.capitalize!
+    end
+    self.title = words.join(" ")
+  end
 end
 
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
+  validates(:name, {presence: true, length: { maximum: 100}})
+  before_save(:titlecase)
+
+private
+  def titlecase
+    words = self.title.split(" ")
+    words.each do |word|
+      word.capitalize!
+    end
+    self.title = words.join(" ")
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe(Store) do
   it { should have_and_belong_to_many(:brands) }
+  it { should validate_presence_of(:name) }  
 end
 
 describe(Brand) do
   it { should have_and_belong_to_many(:stores) }
+  it { should validate_presence_of(:name) }
 end
